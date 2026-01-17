315c79b5b65c5bd0ec0bdf642fb6616ea366c01d
AKJ <avneeshjha1506@gmail.com>
2026-01-17 13:44:24 +0530

    checking wheater the number is prime or not

--- C++/Problems/BASICS/05_PrimeNumberOrNot.cpp
+++ C++/Problems/BASICS/05_PrimeNumberOrNot.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-    int n;
-    cout<<"Enter a Number to check wheater the Number is Prime or Not : ";
+    int n,count;
+    cout<<"Enter a number to check it is prime or not : ";
     cin>>n;
-
-    for(int i=2; i<n;i++){
-        if(n%i==0){
-            cout<<"Not a Prime Number";
-        }
-        else{
-            cout<<"Its a Prime Number";
+    count = 0;
+    for (int i=1; i<=n ;i++){
+        if(n % i == 0){
+            count = count + 1;
         }
     }
+if(count == 2){
+    cout<<"This is a Prime Number";
+}
+else{
+    cout<<"This is not a Prime Number";
+}
 
-    return 0;
+return 0;
 }
