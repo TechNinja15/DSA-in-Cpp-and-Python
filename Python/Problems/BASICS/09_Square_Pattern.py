ca6aef99290cb01099f6837d11b1206d44c57a21
AKJ <avneeshjha1506@gmail.com>
2026-01-19 00:11:33 +0530

    Square pattern problem in python

---
+++ Python/Problems/BASICS/09_Square_Pattern.py
@@ -0,0 +1,62 @@
+'''
+Q1.Example:
+    for n = 4
+    1234
+    1234
+    1234
+    1234
+'''
+
+# n =int(input("Enter a Number : "))
+# for i in range(1,n+1):
+#     for j in range (1,n+1):
+#         print(j, end="")
+#     print()
+
+'''
+Q2. for n = 4 Example:
+    ABCD
+    ABCD
+    ABCD
+    ABCD
+'''
+
+# n=int(input("Enter a Number : "))
+# for i in range(1,n+1):
+#     char ch = 'A'
+#     for j in range(1,n+1):
+#         print(ch)
+#         ch = chr(ord(ch)+1) # here ord() converts character into there ASCII code while chr converts it into Character again and +1 is used to increment the character when it is converted into ASCII code 
+#     print() # this is used to print the next line
+
+'''
+Q3. for n =3 Example: 
+    123
+    456
+    789
+'''
+
+# n=int(input("Enter a Number : "))
+# num = 1
+
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(num,end="")
+#         num = num +1
+#     print()
+
+'''
+Q4. for n = 4 Example:
+    ABC
+    DEF
+    GHI
+'''
+
+# n=int(input("Enter a Number : "))
+# ch = 'A'
+
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(ch,end="")
+#         ch = chr(ord(ch)+1) 
+#     print()
