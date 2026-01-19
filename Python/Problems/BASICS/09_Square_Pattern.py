f0cd6778575cafec64f38df0a4503cfd6950c5b2
AKJ <avneeshjha1506@gmail.com>
2026-01-20 00:09:54 +0530

    Pattern Problem in Python

--- Python/Problems/BASICS/09_Square_Pattern.py
+++ Python/Problems/BASICS/09_Square_Pattern.py
@@ -60,3 +60,93 @@ Q4. for n = 4 Example:
 #         print(ch,end="")
 #         ch = chr(ord(ch)+1) 
 #     print()
+
+'''
+Q5. For n = 4 Example : 
+    *
+    **
+    ***
+    ****
+'''
+
+# a = int(input("Enter a Number : "))
+
+# for i in range(1,a+1):
+#     print("*"*i)
+
+'''
+Q6. for n = 4 Example:
+    1
+    22
+    333
+    4444
+'''
+
+# n = int(input("Enter a Number : "))
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+'''
+Q7. for n = 4 Example:
+    A
+    BB
+    CCC
+    DDDD
+'''
+# n = int(input("Enter a Number : "))
+# ch = "A"
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(ch,end="")
+#     ch = chr(ord(ch)+1) # we kept this outside the inner loop because we want to print the same character in the same row
+#     print()
+
+'''
+Q8. for n = 4 Example : 
+    1
+    21
+    321
+    4321
+'''
+
+# n = int(input("Enter a Number : "))
+
+# for i in range(1,n+1):
+#     for j in range(i, 0, -1):
+#         print(j,end="")
+#     print()
+
+'''
+Q9. for  n=4 Example :
+    1
+    23
+    456
+    78910
+'''
+
+# n = int(input("Enter a Number : "))
+# count = 1
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(count, end="")
+#         count = count + 1
+#     print()
+
+'''
+Q10. for  n=4 Example :
+    A
+    BC
+    DEF
+    GHIJ
+'''
+
+# n = int(input("Enter a Number : "))
+# ch = 'A'
+# for i in range(1,n+1):    
+#     for j in range(1,i+1):
+#         print(ch,end="")
+#         ch= chr(ord(ch)+1)
+#     print()
