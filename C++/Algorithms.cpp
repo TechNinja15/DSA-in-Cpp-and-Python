393f18cd5f3982112598e04cdba6fe4c5f71f9b2
Avneesh Kumar Jha <avneeshjha1506@gmail.com>
2026-04-08 09:29:12 +0530

    Added a Kadane's Algorithm

---
+++ C++/Algorithms.cpp
@@ -0,0 +1,9 @@
+//-------------------------------Kadane's Algorithm----------------------------------------
+
+
+// First Let's disscuss about Sub-Array
+
+// Sub-Array is a contiguous part of an array
+// Example : arr = {1,2,3,4,5}
+// Sub-Array are : {1}, {2}, {3}, {4}, {5}, {1,2}, {2,3}, {3,4}, {4,5}, {1,2,3}, {2,3,4}, {3,4,5}, {1,2,3,4}, {2,3,4,5}, {1,2,3,4,5}
+
