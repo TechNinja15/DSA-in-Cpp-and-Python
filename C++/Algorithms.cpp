e4f759da4f403c2d8c8c8cad7f54c6e081bd48a7
Avneesh Kumar Jha <avneeshjha1506@gmail.com>
2026-04-09 23:52:13 +0530

    Brute Force

--- C++/Algorithms.cpp
+++ C++/Algorithms.cpp
@@ -7,3 +7,25 @@
 // Example : arr = {1,2,3,4,5}
 // Sub-Array are : {1}, {2}, {3}, {4}, {5}, {1,2}, {2,3}, {3,4}, {4,5}, {1,2,3}, {2,3,4}, {3,4,5}, {1,2,3,4}, {2,3,4,5}, {1,2,3,4,5}
 
+This is Brute Force Method to find the maximum sum of sub-array
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr)/sizeof(int);
+    int max = INT_MIN;
+    for(int i=0; i<size; i++){
+        for(int j=i; j<size; j++){
+            int sum = 0;
+            for(int k=i; k<=j; k++){
+                sum += arr[k];
+            }
+            if(sum > max){
+                max = sum;
+            }
+        }
+    }
+    cout<<max<<endl;
+    return 0;
+}
